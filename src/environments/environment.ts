4382e608102652008f9fb0e0dc47d4dec5196072
Chirapong01 <74916492+Chirapong01@users.noreply.github.com>
2024-09-01 13:42:59 +0700

    change api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
-    production: false,
-    apiUrl: "http://localhost:5020/api/",
+    production: true,
+    // apiUrl: "http://localhost:8081/api/",
+    apiUrl: "http://a6c5410076777474fa7fc5b5effac493-241426457.ap-southeast-1.elb.amazonaws.com/api/",
     secretKey: "6D256CBA2B6231CEiKf9vBVMW0Qiu6EWgzwU7PyCdD0BLxv7ks4kTe4fXvGPDYsS3QT3wugV4ReGopt314A63456A19F1"
 };
 1
