c5a434785a3834d437353aea82873237b1c02af3
Chirapong Phomphoo <Chirapong@PLANTOYS.NET>
2024-09-12 16:40:45 +0700

    last comit plantoys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
     // apiUrl: "http://localhost:8081/api/",
-    // apiUrl: "http://a6c5410076777474fa7fc5b5effac493-241426457.ap-southeast-1.elb.amazonaws.com/api/",
-    apiUrl: "http://localhost:5020/api/", //onlocalhost
+    apiUrl: "https://ecomapi-580961450.ap-southeast-1.elb.amazonaws.com/api/",
+    // apiUrl: "http://localhost:5020/api/", //onlocalhost
     secretKey: "6D256CBA2B6231CEiKf9vBVMW0Qiu6EWgzwU7PyCdD0BLxv7ks4kTe4fXvGPDYsS3QT3wugV4ReGopt314A63456A19F1"
 };
 1
